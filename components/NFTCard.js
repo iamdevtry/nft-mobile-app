5a2e47b71ec554cd6a2e995eddc28d34065a62e7
iamdevtry <tranbinhphongcmg@gmail.com>
2022-12-06 22:53:27 +0700

    complete ntf card

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -2,6 +2,9 @@ import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+
+import { CircleButton, RectButton } from './Button';
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo';
 const NFTCard = ({ data }) => {
     const navigation = useNavigation();
     return (
@@ -25,8 +28,32 @@ const NFTCard = ({ data }) => {
                         borderBottomRightRadius: SIZES.font,
                     }}
                 />
+                <CircleButton imgUrl={assets.heart} right={10} top={10} />
             </View>
-            <Text>NFTCard</Text>
+            <SubInfo />
+            <View style={{ width: '100%', padding: SIZES.font }}>
+                <NFTTitle
+                    title={data.name}
+                    subTitle={data.creator}
+                    titleSize={SIZES.large}
+                    subTitleSize={SIZES.small}
+                />
+                <View
+                    style={{
+                        marginTop: SIZES.font,
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
+                    <EthPrice price={data.price} />
+                    <RectButton
+                        minWidth={120}
+                        fontSize={SIZES.font}
+                        handlePress={() => navigation.navigate('Details', { data })}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
